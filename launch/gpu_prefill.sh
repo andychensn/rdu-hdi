0e8526f677e443aab8046fa69a68fb19e20cdf04
Andy Chen <andy.chen@sambanovasystems.com>
2026-06-30 07:19:04 -0700

    fix: set UCX_MODULE_DIR to prevent system libuct_ib from being loaded

--- launch/gpu_prefill.sh
+++ launch/gpu_prefill.sh
@@ -46,6 +46,7 @@ if [[ "${1:-}" == "--inner" ]]; then
         -e "VLLM_PD_STAGE_TIMING=1" \
         -e "FLASHINFER_DISABLE_VERSION_CHECK=1" \
         -e "VLLM_USE_DEEP_GEMM=0" \
+        -e "UCX_MODULE_DIR=/opt/ucx/lib/ucx" \
         -e "HF_HOME=$GPU_CACHE_ROOT/huggingface" \
         -e "VLLM_CACHE_ROOT=$GPU_CACHE_ROOT/vllm" \
         -e "TRITON_CACHE_DIR=$GPU_CACHE_ROOT/triton" \
